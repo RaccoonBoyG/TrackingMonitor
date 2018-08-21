7d9c3ce7578880f54351adcf451e4dc718a1a263
anonimD08 <asdebug@yandex.ru>
2018-08-21 15:51:45 +0500

    test1

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    username = models.CharField("Имя пользователя", blank=True, null=True, max_length=512)
+    event_type = models.CharField("Тип события", null=True, blank=True, max_length=512)
+    ip = models.CharField("IP адрес пользователя", blank=True, null=True, max_length=512)
+    agent = models.CharField("Информация о браузере", blank=True, null=True, max_length=512)
+    description = models.TextField("Описание платформы", blank=True, null=True)
+    ogrn = models.CharField("ОГРН", blank=True, null=True, max_length=512)
+    contacts = models.TextField("Контакты", blank=True, null=True) 
