7e380fa3d276f6fef6c04f87faaa5f73cb0cc8e2
anonimD08 <asdebug@yandex.ru>
2018-08-28 17:51:34 +0500

    test_spark

--- monitor/views.py
+++ monitor/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render, redirect
 from .models import \
     Track, TrackCourse
 
+from pyspark import SparkContext, SparkConf
+
 def upload_from_json(request):
-        #track = json.loads(request.POST.get("json_value", None))
-        return render(request, 'upload_from_json.html')
+    conf = SparkConf().setAppName('MyFirstStandaloneApp')
+    sc = SparkContext(conf=conf)
+    #track = json.loads(request.POST.get("json_value", None))
+    logRDD = sc.textFile("/home/alex/big_data_edx/tracking.log")
+    test = logRDD.first()
+    context = {
+        'first_obj': test,
+    }
+    return render(request, 'upload_from_json.html', context)
